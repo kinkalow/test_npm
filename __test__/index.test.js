2054f91219339bd280275563753bc7329bc984ca
kinkalow <kinkalow90@gmail.com>
2021-06-01 12:32:17 +0900

    first commit

---
+++ __test__/index.test.js
@@ -0,0 +1,16 @@
+const fizzbuzz = require("../index");
+
+describe("fizzbuzz", () => {
+  it("returns FizzBuzz", () => {
+    expect(fizzbuzz(30)).toStrictEqual("FizzBuzz");
+  });
+  it("returns Fizz", () => {
+    expect(fizzbuzz(9)).toStrictEqual("Fizz");
+  });
+  it("returns Buzz", () => {
+    expect(fizzbuzz(7)).toStrictEqual("Buzz");
+  });
+  it("returns the value", () => {
+    expect(fizzbuzz(30)).toStrictEqual("7");
+  });
+});
